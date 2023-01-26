f7b8099d25a6e17b929e36b2f4535d63b18543db
Jakub Karaś <jakub.karas@onet.pl>
2023-01-26 15:33:38 +0100

    added logic to change table content laungage

--- src/components/DataTable/DataTable.styled.ts
+++ src/components/DataTable/DataTable.styled.ts
@@ -3,7 +3,7 @@ import { DataGrid } from "@mui/x-data-grid"
 
 export const DataGridWrapper = styled(DataGrid)`
 width: 100%;
-max-width: 41rem;
+max-width: 44rem;
 height: 475px;
 margin: 0 auto;
 
@@ -14,4 +14,10 @@ margin: 0 auto;
 & .MuiDataGrid-columnHeaderTitle {
   text-align: center;
 }
+`
+
+export const Button = styled('button')`
+  width: 300px;
+  height: 300px;
+  background-color: blue;
 `
--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,8 +1,10 @@
-import { GridColDef } from "@mui/x-data-grid"
-import rows from "src/data/MedalsContentRows.json"
+import React from "react"
+import { GridColDef, plPL } from "@mui/x-data-grid"
+import polishRows from "src/data/MedalsContentRowsPL.json"
+import englishRows from "src/data/MedalsContentRowsEN.json"
 import * as S from "src/components/DataTable/DataTable.styled"
 
-const columns: GridColDef[] = [
+const englishColumns: GridColDef[] = [
   {
     field: "id",
     headerName: "Position",
@@ -26,14 +28,14 @@ const columns: GridColDef[] = [
     field: "silver",
     headerName: "Silver medals",
     type: "number",
-    width: 120,
+    width: 130,
     editable: true
   },
   {
     field: "bronze",
     headerName: "Bronze medals",
     type: "number",
-    width: 120,
+    width: 135,
     editable: true
   },
   {
@@ -45,19 +47,97 @@ const columns: GridColDef[] = [
   }
 ]
 
+const polishColumns: GridColDef[] = [
+  {
+    field: "id",
+    headerName: "Pozycja",
+    width: 80,
+    editable: true
+  },
+  {
+    field: "country",
+    headerName: "Kraj",
+    width: 125,
+    editable: true
+  },
+  {
+    field: "gold",
+    headerName: "Złote medale",
+    type: "number",
+    width: 110,
+    editable: true
+  },
+  {
+    field: "silver",
+    headerName: "Srebrne medale",
+    type: "number",
+    width: 130,
+    editable: true
+  },
+  {
+    field: "bronze",
+    headerName: "Brązowe medale",
+    type: "number",
+    width: 135,
+    editable: true
+  },
+  {
+    field: "all",
+    headerName: "Wszystkie",
+    type: "number",
+    width: 90,
+    editable: true
+  }
+]
+
 export default function DataTable() {
+  const [columnsLanguage, setColumnsLanguage] = React.useState(englishColumns)
+  const [rowsLanguage, setRowsLanguage] = React.useState(englishRows)
+  const [toolbarLanguage, setToolbarLanguage] = React.useState({})
+  function ChangeLaungage(lang: string) {
+    const toolbarLaungage = plPL.components.MuiDataGrid.defaultProps.localeText
+    if (lang === "en") {
+      setColumnsLanguage(englishColumns)
+      setRowsLanguage(englishRows)
+      setToolbarLanguage({})
+    }
+    if (lang === "pl") {
+      setColumnsLanguage(polishColumns)
+      setRowsLanguage(polishRows)
+      setToolbarLanguage(toolbarLaungage)
+    } else {
+      setColumnsLanguage(englishColumns)
+      setRowsLanguage(englishRows)
+      setToolbarLanguage({})
+    }
+  }
   return (
     <>
       <S.DataGridWrapper
-        rows={rows}
-        columns={columns}
+        rows={rowsLanguage}
+        columns={columnsLanguage}
         initialState={{
           pagination: {
             pageSize: 7
           }
         }}
         disableSelectionOnClick
+        localeText={toolbarLanguage}
       />
+      <S.Button
+        onClick={() => {
+          ChangeLaungage("en")
+        }}
+      >
+        English
+      </S.Button>
+      <S.Button
+        onClick={() => {
+          ChangeLaungage("pl")
+        }}
+      >
+        Polski
+      </S.Button>
     </>
   )
 }
