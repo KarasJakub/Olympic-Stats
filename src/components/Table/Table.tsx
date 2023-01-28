101ed6b469de1c135abe535dbd3ba2943da47670
Jakub Karaś <jakub.karas@onet.pl>
2023-01-28 17:18:00 +0100

    added rest of sorting logic

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -9,40 +9,62 @@ type SortKeys = keyof Data[0]
 
 type SortOrder = "ascn" | "desc"
 
-function sortData({
-  tableData,
-  sortKey,
-  reverse
-}: {
+type SortDataTypes = {
   tableData: Data
   sortKey: SortKeys
   reverse: boolean
-}) {
-  if (!sortKey) return tableData
+}
 
-  const sortedData = MedalsEN.sort((a, b) => {
-    return a[sortKey] > b[sortKey] ? 1 : -1
+function sortData(props: SortDataTypes) {
+  if (!props.sortKey) return props.tableData
+
+  const toSort = [...MedalsEN]
+  // console.log(MedalsEN)
+  // console.log(toSort.reverse())
+  const sortedData = toSort.sort((a, b) => {
+    return a[props.sortKey] > b[props.sortKey] ? 1 : -1
   })
 
-  if (reverse) {
+  if (props.reverse) {
     return sortedData.reverse()
   }
 
   return sortedData
 }
 
-function SortButton() {
-  return <button>▲</button>
+function SortButton({
+  sortOrder,
+  columnKey,
+  sortKey,
+  onClick
+}: {
+  sortOrder: SortOrder
+  columnKey: SortKeys
+  sortKey: SortKeys
+  onClick: () => void
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`${
+        sortKey === columnKey && sortOrder === "desc"
+          ? "sort-button sort-reverse"
+          : "sort-button"
+      }`}
+    >
+      ▲
+    </button>
+  )
 }
 
 export default function Table({ data }: { data: Data }) {
   const [sortKey, setSortKey] = React.useState<SortKeys>("id")
   const [sortOrder, setSortOrder] = React.useState<SortOrder>("ascn")
 
-  const headers = [
+  const headers: { key: SortKeys; label: string }[] = [
     { key: "id", label: "Position" },
     { key: "country", label: "Country" },
-    { key: "flag", label: "Flag" },
+    { key: "image", label: "Flag" },
     { key: "gold", label: "Gold Medals" },
     { key: "silver", label: "Silver Medals" },
     { key: "bronze", label: "Bronze Medals" },
@@ -54,6 +76,11 @@ export default function Table({ data }: { data: Data }) {
     [data, sortKey, sortOrder]
   )
 
+  function changeSort(key: SortKeys) {
+    setSortOrder(sortOrder === "ascn" ? "desc" : "ascn")
+    setSortKey(key)
+  }
+
   return (
     <>
       <S.TableWrapper>
@@ -62,7 +89,14 @@ export default function Table({ data }: { data: Data }) {
             <S.Row>
               {headers.map(item => (
                 <>
-                  <S.Headers key={item.key}>{item.label}</S.Headers>
+                  <S.Headers key={item.key}>
+                    {item.label}{" "}
+                    <SortButton
+                      columnKey={item.key}
+                      onClick={() => changeSort(item.key)}
+                      {...{ sortOrder, sortKey }}
+                    />
+                  </S.Headers>
                 </>
               ))}
             </S.Row>
